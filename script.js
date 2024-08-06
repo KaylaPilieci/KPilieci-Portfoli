da320a8f32998638f1f496e7b5c36692764a3296
KaylaPilieci <Kaylap214@nycstudents.net>
2024-08-06 16:04:22 +0000

    Age Verification

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-console.log('Hello World!')
+// console.log('Hello World')
 
-var prompt = prompt('What is your name?')
+// var promopt= prompt('what is your name?')
 
-console.log(prompt)
+// console.log(prompt)
+
+
+ function verifyAge(age){
+var age = prompt('what is your age');
+ console.log(age)
+   
+ if (age < 18){
+  alert('You are a minor!')
+ } else if (age >= 18 && age <= 60) {
+  alert('You are an adult!')
+ } else {
+  alert('You are a senior citizen!')
+ }
+
+  
+}
