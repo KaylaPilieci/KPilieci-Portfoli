ec2e5083b94667dfe0b93ed9455f7b6c7e786500
KaylaPilieci <Kaylap214@nycstudents.net>
2024-08-07 16:50:40 +0000

    PlantID

--- script.js
+++ script.js
@@ -15,7 +15,22 @@ var age = prompt('what is your age');
   alert('You are an adult!')
  } else {
   alert('You are a senior citizen!')
- }
+ } 
+}
 
-  
+
+function uploadAndIdentifyPlant() {
+// get the photo from the frontend
+var photoInput = document.getElementById('photoInput');
+  console.log(photoInput)
+  console.log(photoInput.files[0])
+
+  // if no photo is uploaded, then alert the user to upload a photo
+if (photoInput.files.length === 0) {
+  alert('Please select a photo to upload.');
+  return;
+  }
+
+// select the first file from the files folder/array from the upload button
+  var selectedFile = photoInput.files[0];
 }
